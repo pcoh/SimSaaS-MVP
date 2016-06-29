b1048ef86ac148e32d6fd32bdba29a56eacb4abe
Patrick Cohen <patrick.cohen.at@gmail.com>
2016-06-28 22:33:32 -0700

    Dynamically set height of "Workspace" to fill available space and make contents of "workspace" (SimControls, Tables & plots) scroll independently from rest of page

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,10 +1,17 @@
 function resizeTrackArea() {
   $("#divTrackContainer").height($( window ).height());
 }
+function resizeWorkSpace() {
+	var WSheight = $( window ).height()-$("#divEventBanner").height()-$("#navTrackNavBar").height();
+  	$("#divWorkspaceContainer").height(WSheight);
+  	//alert($( window ).height() + " "+$("#divEventBanner").height() + " " + $("#navTrackNavBar").height()+ " " + WSheight);
+}
 
 $( document ).ready(function() {
     resizeTrackArea();
+    resizeWorkSpace();
 });
 $( window ).resize(function() {
 	resizeTrackArea();
+	resizeWorkSpace();
 })

80c4bf1ea3671cfeaa3a853f4a8dd9fbd74961a1
Patrick Cohen <patrick.cohen.at@gmail.com>
2016-06-29 15:19:46 -0700

    Make plot areas drag and drop-able

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,10 +1,17 @@
+plotCount = 1;
+stdPlotOpcacity = $(".divPlotContainer").css("opacity");
+dragPlotOpacity = '0.4';
+function createPlotContainer(plotCount){
+	plotContHTML = "<div class=\"col-sm-12 fuchsia divPlotContainer\" draggable=\"true\">PlotContainer" + plotCount + "<a href=\"#\"><span class=\"removePlot grey\">Remove Plot</span></a></div>";
+	return plotContHTML;
+}
 function resizeTrackArea() {
   $("#divTrackContainer").height($( window ).height());
 }
 function resizeWorkSpace() {
 	var WSheight = $( window ).height()-$("#divEventBanner").height()-$("#navTrackNavBar").height();
   	$("#divWorkspaceContainer").height(WSheight);
-  	//alert($( window ).height() + " "+$("#divEventBanner").height() + " " + $("#navTrackNavBar").height()+ " " + WSheight);
+  	
 }
 
 $( document ).ready(function() {
@@ -17,15 +24,95 @@ $( window ).resize(function() {
 })
 
 $('.addPlot').on('click',  function() {
-     // $( "<div class=\"col-sm-12 fuchsia divPlotContainer myhidden\" >PlotContainer<a href=\"#\"><span class=\"removePlot grey\">Remove Plot</span></a></div>" ).insertBefore( $(this).parent(), function(){
-     // 	$(this).closest(".divPlotContainer" ).show();
-     // });
-
-	$( "<div class=\"col-sm-12 fuchsia divPlotContainer\" >PlotContainer<a href=\"#\"><span class=\"removePlot grey\">Remove Plot</span></a></div>" ).insertBefore( $(this).parent()).slideDown();
+     
+	plotCount = plotCount +1;
+	plotContHTML = createPlotContainer(plotCount);
+	$( plotContHTML ).insertBefore( $(this).parent()).slideDown();
+	addListenerToPlots();
+	
 });
 
 $('#divWorkspaceContainer').on('click', 'div a .removePlot', function() {
 	$(this).parent().parent().slideUp(400, function() {
 		$(this).remove();
 	});
+	addListenerToPlots();
+	plotCount = plotCount -1;
 });
+
+
+function handleDragStart(e) {
+	this.style.opacity = dragPlotOpacity; 
+	dragSrcEl = this;
+
+  	e.dataTransfer.effectAllowed = 'move';
+  	e.dataTransfer.setData('text/html', this.innerHTML);
+}
+function handleDragEnter(e) {
+  // this / e.target is the current hover target.
+	if (dragSrcEl != this) {
+	  	//this.classList.add('over');
+		dropZone = "<div class=\"col-sm-12 dropZone\">Drop here</div>";
+		$newDropZone = $( dropZone ).insertBefore( $(this));
+		return $newDropZone;
+	}
+}
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault(); 
+  }
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+
+function handleDragLeave(e) {
+  //this.classList.remove('over');  // this / e.target is previous target element.
+  if (dragSrcEl != this) {
+  	$newDropZone.remove();
+  }
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation(); // stops the browser from redirecting.
+  }
+  
+  // Don't do anything if dropping the same plot we're dragging.
+  if (dragSrcEl != this) {
+    $cutPlot = $(dragSrcEl).detach();
+
+    $cutPlot.insertBefore($(this)).fadeTo(1,0,function() {
+    	$cutPlot.hide(1,function(){
+    		$cutPlot.slideDown(500,function() {
+    			$cutPlot.fadeTo(100,stdPlotOpcacity);
+    		})
+    	});    	
+  	})
+  }
+  return false;
+}
+function handleDragEnd(e) {
+  // this/e.target is the source node.
+	$newDropZone.remove();
+  [].forEach.call(plotContains, function (plCont) {
+    //plCont.classList.remove('over');
+    plCont.style.opacity = stdPlotOpcacity; 
+    //plCont.classList.remove('over'); 
+  });
+}
+
+
+function addListenerToPlots(){
+	plotContains = document.querySelectorAll('.row .divPlotContainer');
+	[].forEach.call(plotContains, function(plCont) {
+		plCont.addEventListener('dragstart', handleDragStart, false);	  	
+	  	plCont.addEventListener('dragenter', handleDragEnter, false);
+	  	plCont.addEventListener('dragover', handleDragOver, false);
+		plCont.addEventListener('dragleave', handleDragLeave, false);
+		plCont.addEventListener('drop', handleDrop, false);
+		plCont.addEventListener('dragend', handleDragEnd, false);
+	});
+	//return plotContains;
+}
+addListenerToPlots();
